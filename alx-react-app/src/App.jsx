2e642208011231e2100b0663da0d81de985beda3
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-07-13 20:02:50 +0100

    feat: add UserProfile component to display user information in App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -4,12 +4,14 @@ import WelcomeMessage from "./components/welcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
       <WelcomeMessage />
       <Header />
+      <UserProfile name="John Doe" age={30} bio="Software Engineer" />
       <MainContent />
       <Footer />
     </>
