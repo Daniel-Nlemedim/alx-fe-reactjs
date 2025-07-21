0d023764208e09e8c636f97df66f56e118c2ccb0
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-07-21 16:28:18 +0100

    feat: add inline styles to Header component for improved styling

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 function Header() {
+  const headerStyle = {
+    backgroundColor: "navy",
+    color: "white",
+    textAlign: "center",
+  };
   return (
-    <header>
+    <header style={headerStyle}>
       <h1>My Favorite Cities</h1>
     </header>
   );
