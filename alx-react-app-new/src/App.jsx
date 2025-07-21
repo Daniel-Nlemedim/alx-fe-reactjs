e2a839738ecc0b41f1ad14b471bf2241af10c0fc
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-07-21 16:44:35 +0100

    feat: add Counter component for interactive counting functionality

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,11 +3,13 @@ import "./App.css";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
     <>
+      <Counter />
       <Header />
       <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
       <MainContent />
