2b990dc41cae38e942c7cb4682a2eca00028edd1
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-07-29 22:28:25 +0100

    created the my-company site'

---
+++ my-company/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div></div>
+      <h1>Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
