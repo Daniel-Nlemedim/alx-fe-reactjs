73deb9bf12aed0c31cb8fb86acb8077a2f256fe1
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-07-29 23:14:03 +0100

    added some new updates

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Services from "./Service";
 import Contact from "./Contact";
+import About from "./About";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
