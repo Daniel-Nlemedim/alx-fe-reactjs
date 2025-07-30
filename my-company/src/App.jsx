2f7cfe950afd040e70252f5b88fd610a8d9e5191
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-07-30 01:41:09 +0100

    minor changes

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Services from "./Service";
-import Contact from "./Contact";
+import Contact from "./components/Contact";
 import About from "./About";
 import Navbar from "./components/Navbar";
 import "./App.css";
