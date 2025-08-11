7dfc3b608f003b38999e520511870904508c96dc
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-08-11 14:41:06 +0100

    feat: implement routing for RecipeList and SearchBar components

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import SearchBar from "./components/SearchBar";
@@ -7,9 +7,13 @@ import "./App.css";
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<RecipeList />} />
+          <Route path="/search" element={<SearchBar />} />
+        </Routes>
+      </Router>
       <AddRecipeForm />
-      <RecipeList />
-      <SearchBar />
     </>
   );
 }
