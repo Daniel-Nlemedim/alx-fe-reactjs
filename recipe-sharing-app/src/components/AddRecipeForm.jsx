9bd42b06d2eff41b0d56f1c1547149e7ddecd0d4
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-08-11 14:17:14 +0100

    feat: update styling and structure of AddRecipeForm and RecipeList components; remove unused App.css and index.css

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,8 +1,53 @@
 // AddRecipeForm component
 import { useState } from "react";
-import { useRecipeStore } from "./recipeStore";
+import useRecipeStore from "./recipeStore";
 
 const AddRecipeForm = () => {
+  const style = {
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "column",
+    maxWidth: "400px",
+    margin: "0 auto",
+    padding: "20px",
+    paddingTop: "15px",
+    gap: "12px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+    border: "1px solid #ccc",
+    borderRadius: "8px",
+    backgroundColor: "#f9f9f9",
+  };
+
+  const styleBtn = {
+    padding: "10px",
+    backgroundColor: "#4CAF50",
+    color: "white",
+    border: "none",
+    borderRadius: "4px",
+  };
+
+  const styleInput = {
+    marginBottom: "12px",
+    height: "30px",
+    paddingTop: "6px",
+    focus: "none",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    boxShadow: "0 1px 2px rgba(0, 0, 0, 0.1)",
+    transition: "border-color 0.3s ease",
+  };
+
+  const styleTextarea = {
+    marginBottom: "12px",
+    height: "100px",
+    paddingTop: "6px",
+    focus: "none",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    boxShadow: "0 1px 2px rgba(0, 0, 0, 0.1)",
+    transition: "border-color 0.3s ease",
+  };
+
   const addRecipe = useRecipeStore((state) => state.addRecipe);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
@@ -15,19 +60,23 @@ const AddRecipeForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} style={style}>
       <input
+        style={styleInput}
         type="text"
         value={title}
         onChange={(e) => setTitle(e.target.value)}
         placeholder="Title"
       />
       <textarea
+        style={styleTextarea}
         value={description}
         onChange={(e) => setDescription(e.target.value)}
         placeholder="Description"
       />
-      <button type="submit">Add Recipe</button>
+      <button type="submit" style={styleBtn}>
+        Add Recipe
+      </button>
     </form>
   );
 };
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,11 +1,21 @@
 // RecipeList component
-import { useRecipeStore } from "./recipeStore";
+import useRecipeStore from "./recipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
+  const style = {
+    display: "flex",
+    flexDirection: "column",
+    padding: "20px",
+    gap: "12px",
+    alignItems: "center",
+    maxWidth: "600px",
+    margin: "0 auto",
+  };
+
   return (
-    <div>
+    <div style={style}>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
