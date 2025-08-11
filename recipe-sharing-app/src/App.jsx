f5ea2d5528116b8df9a702e2421a127ce98d06b2
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-08-11 14:25:22 +0100

    feat: add SearchBar component and integrate search functionality in recipe store

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 // import { useState } from "react";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import SearchBar from "./components/SearchBar";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <AddRecipeForm />
       <RecipeList />
+      <SearchBar />
     </>
   );
 }
