25d5f4e27a145b300fdb2f7d3fdb1c2da71d94a5
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-08-18 18:11:28 +0100

    style: update UserProfile component with new profile image and enhanced hover effects

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
+    <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-md hover:shadow-xl transition-shadow duration-300">
       <img
-        src="https://via.placeholder.com/150"
+        src="/profile.png"
         alt="User"
-        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full object-cover mx-auto"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full object-cover mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-lg md:text-xl text-blue-800 my-4 font-bold">
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 font-bold hover:text-blue-400 transition-colors duration-300">
         John Doe
       </h1>
       <p className="text-sm md:text-base text-gray-600">
