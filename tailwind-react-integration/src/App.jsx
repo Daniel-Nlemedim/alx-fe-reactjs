486d6e4a6f1db0710a7d361c8e7c8e6675174cb6
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-08-18 15:21:30 +0100

    feat: initialize Tailwind CSS with React integration
    - Added package.json for project setup with dependencies and scripts.
    - Created App component with basic structure and Tailwind CSS styling.
    - Added index.css to include Tailwind's base, components, and utilities.
    - Set up main entry point for React application.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-4xl foh1-bold">Hello, Tailwind CSS!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
