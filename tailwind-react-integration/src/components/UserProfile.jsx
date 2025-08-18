77602e5de502a27639058ebbdd6b4c47bc298be2
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-08-18 16:49:37 +0100

    style: adjust max-width for UserProfile component layout

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile  bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 sm:p-4 md:p-8 lg:p-8">
+    <div className="user-profile  bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 sm:p-4 md:p-8 lg:p-8  md:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
