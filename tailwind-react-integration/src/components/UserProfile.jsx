276a42c033f7324f253911269db200d8e1a8eb01
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-08-18 16:47:13 +0100

    style: update padding and image class for improved layout in UserProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
   return (
-    <div className="user-profile  bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 sm:p-4 md:p-6">
+    <div className="user-profile  bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 sm:p-3 md:p-8 lg:p-8">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-36 h-36 rounded-full mx-auto"
+        className="w-36 rounded-full mx-auto sm:w-24 md:w-36 h-36"
       />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
