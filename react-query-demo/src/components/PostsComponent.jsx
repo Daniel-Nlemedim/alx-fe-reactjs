6728a7fb5d9655cffa5352a0fa372b69abe03607
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-09-02 19:26:34 +0100

    quick fix on the postscomponent

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -12,16 +12,15 @@ async function fetchPosts() {
 
 function PostsComponent() {
   // Using useQuery with caching and other options
-  const { data, isLoading, isError, error, refetch, isFetching } = useQuery(
-    ["posts"],
-    fetchPosts,
-    {
-      cacheTime: 1000 * 60 * 5, // 5 minutes (how long data stays in cache before garbage collection)
-      refetchOnWindowFocus: false, // Disable auto refetch when tab regains focus
-      keepPreviousData: true, // Keeps old data while fetching new data
-      staleTime: false,
-    }
-  );
+  const { data, isLoading, isError, error, refetch, isFetching } = useQuery({
+    queryKey: ["posts"],
+    queryFn: fetchPosts,
+
+    cacheTime: 1000 * 60 * 5, // 5 minutes (how long data stays in cache before garbage collection)
+    refetchOnWindowFocus: false, // Disable auto refetch when tab regains focus
+    keepPreviousData: true, // Keeps old data while fetching new data
+    staleTime: false,
+  });
 
   if (isLoading) return <p>Loading posts...</p>;
   if (isError) return <p>Error: {error.message}</p>;
