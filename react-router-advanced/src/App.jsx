cb5d795600c3e3a910348a9beee2a39805f96e99
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-09-03 16:11:17 +0100

    integrated the router dom

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,10 @@
-import { Router } from "react-router-dom";
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+} from "react-router-dom";
 function App() {
   return (
     <>
