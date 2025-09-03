354c0b470a92761036f6c4cb7f1ddc5e32f91dc4
Daniel-Nlemedim <nzubenlems2002@gmail.com>
2025-09-03 16:15:23 +0100

    added the profileseting component also added the profiledetails component also integrated router-dom in the profile component "

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,6 @@
+import { Routes, Route } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
+
+function Profile() {}
+export default Profile;
